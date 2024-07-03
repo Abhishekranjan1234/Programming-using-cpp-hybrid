f3a15f8a934a9f8decea72a5da4b1423b34961e4
ABHISHEK RANJAN <118338987+Abhishekranjan1234@users.noreply.github.com>
2024-07-03 14:14:25 +0530

    Update Lecture 22-Cipher School.cpp

--- Lecture 22-Cipher School.cpp
+++ Lecture 22-Cipher School.cpp
@@ -1,31 +1,34 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
+
+int main(){
+	
+	//	char r,o,h,i,t;
+	//	
+	//	cin>>r>>o>>h>>i>>t;
+    char name[1000];
+    cout<<"Enter your name: (Note: Enter # after end of your name: )";
+    int i=0;
+    while(1){
+    	cin>>name[i];
+    	if(name[i] == '#'){
+    		break;
+		}
+    	i++;  //increasing by 1 every time
+	}
     
-int main()
-{
-    string a;
-    string b;
-    string c;
     
-    // int a;
-    // int b;
-    // int c;
-    
-    // cin>>a>>b;
-    
-    //cin>>a
-    
-    getline(cin,a);
-    
-    cout<<"the string which tou give as iput are "<<a<<b;
-    cout<<"your full name "<<a;    
-    // string c;
-    c=a+b;
-    
-    cout<<"the combined word is : "<<c<<endl;
-    
-    cout<<"length of cobined name is "<<c.length();
-    
-    return 0;
+	//    for(int i=0;i<6;i++){
+	//    	cin>>name[i];
+	//	}
+	//	cout<<"name is: ";
+	//	for(int i=0;i<6;i++){
+	//    	cout<<name[i];
+	//	}
+	i=0;  //again initialize to 0 to print from beginning
+    while(name[i] != '#'){
+    	cout<<name[i];
+    	i++;
+	}
+	return 0;
 }
